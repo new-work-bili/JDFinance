72cfe85dd6a5cbac19a175f56d6ed62ad90cad2d
LPP <1655170926@qq.com>
2019-07-04 12:53:27 +0800

    setup webpack3.10

---
+++ app/js/home/index.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div :class="$style.home">	<!-- 原本是 class='home';这里是使用了css Module;但是为什么？ -->
+        <div>Home</div>
+		<div :class="$style.test"></div>
+		<p class="title">title</p>
+    </div>
+</template>
+
+<script>
+export default {	//为什么这个地方不输出也能
+	/* name:"Home" */
+
+}
+</script>
+
+<style lang="scss" module>	//module表示使用css Module 即css模块化
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 160px;
+  }
+  .test{
+	  background-color: red;
+	  width: 50px;
+	  height: 100px;
+	  background: red;
+  }
+</style>
